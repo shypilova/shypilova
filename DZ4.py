ccf75d937fc9cca80942fea58ab7f7d79578ce6d
Maryna.Shypilova <shypilovamarina@gmail.com>
2024-02-27 19:34:32 +0100

    add second project

---
+++ DZ4.py
@@ -0,0 +1,16 @@
+x = int(input('Please enter first number:'))
+y = int(input('Please enter second number:'))
+z = input('Please enter action:')
+
+if z == '*':
+    print(int(x * y))
+elif z == '+':
+    print(int(x + y))
+elif z == '-':
+    print(int(x - y))
+
+if z == '/':
+    if y == 0:
+        print('You can not divide by 0')
+    else:
+        print(int(x / y))
