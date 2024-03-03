013fb9466998ee17226a4a756e14650f91ca1fef
Maryna.Shypilova <shypilovamarina@gmail.com>
2024-03-03 22:56:23 +0100

    dz7

--- DZ7.py
+++ DZ7.py
@@ -1,9 +1,9 @@
-list_1 = [0, 1, 0, 12, 3]
-i = 0
-# while i < len(list_1):
-#     l = list(i)
-#     if l == 0:
-counter = counter(i)
-
-
-#while list(:-1)
+list_1 = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+if 0 in list_1:
+    for i, el in enumerate(list_1):
+        if el == 0:
+            list_1.remove(el)
+            list_1.append(0)
+else:
+    print('no 0 in list')
+print(list_1)
