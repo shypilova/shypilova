ccf3b4cbb51197f90bad53dc5d1e3a73040f7b88
Maryna.Shypilova <shypilovamarina@gmail.com>
2024-03-10 15:31:44 +0100

    dz15

--- DZ14.py
+++ DZ14.py
@@ -1,6 +1,19 @@
-string = "это пример строки....wow!!!"
-a = string.zfill(30)
-print ("str.zfill : ", a)
-import string
-print(string.ascii_lowercase)
-print(string.ascii_letters)
+x = '999' #input('Enter some letter')
+y = tuple(x)
+mult = 1
+for el in y:
+    mult *= int(el)
+
+print(mult)
+print(y)
+print(type(y))
+print(type(y[0]))
+
+
+
+# string = "это пример строки....wow!!!"
+# a = string.zfill(30)
+# print ("str.zfill : ", a)
+# import string
+# print(string.ascii_lowercase)
+# print(string.ascii_letters)
---
+++ DZ15.py
@@ -0,0 +1,17 @@
+x = int('1900800')
+d1 = ("день", "дні", "днів")
+day = 86400
+hour = 3600
+days = x // day
+hours = (x - days * day) // 3600
+rest = (x - days * day) - hours * hour
+minute = rest // 60
+seconds = rest - minute * 60
+my_dict = {"hours": hours, "minute": minute, "seconds": seconds}
+if days % 10 == 1 and days % 100 != 11:
+        a = d1[0]
+elif 2 <= days % 10 <= 4 and (days % 100 < 10 or days % 100 >= 20):
+        a = d1[1]
+else:
+        a = d1[2]
+print(days, a, ',', my_dict['hours'], ':', my_dict['minute'], ':', my_dict['seconds'])
