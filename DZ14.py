ff261c123df89461e3135a9587b16af5860d853b
Maryna.Shypilova <shypilovamarina@gmail.com>
2024-03-13 17:23:28 +0100

    dz14

--- DZ14.py
+++ DZ14.py
@@ -1,19 +1,8 @@
-x = '999' #input('Enter some letter')
-y = tuple(x)
+x = '999' # input('Enter some letter')
+y = list(x)
 mult = 1
 for el in y:
     mult *= int(el)
 
 print(mult)
-print(y)
-print(type(y))
-print(type(y[0]))
-
-
-
-# string = "это пример строки....wow!!!"
-# a = string.zfill(30)
-# print ("str.zfill : ", a)
-# import string
-# print(string.ascii_lowercase)
-# print(string.ascii_letters)
+print(type(mult))
