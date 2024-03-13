7bfab275f091dd35f0d275a81c786d904a393c12
Maryna.Shypilova <shypilovamarina@gmail.com>
2024-03-13 20:20:25 +0100

    dz14

--- DZ14.py
+++ DZ14.py
@@ -1,8 +1,17 @@
-x = '999' # input('Enter some letter')
-y = list(x)
-mult = 1
-for el in y:
-    mult *= int(el)
-
-print(mult)
-print(type(mult))
+x = int(input('Enter digit:')) # input('Enter some letter')
+def digits(a):
+    my_list = []
+    while a != 0:
+        my_list.append(a % 10)
+        a = a // 10
+    return my_list
+def persistence(x):
+    while x > 9:
+        d = digits(x)
+        mul = 1
+        for el in d:
+            mul = mul * el
+        x = mul
+        test = ('*'.join(map(str, d)) + '=' + str(mul))
+    print(mul)
+persistence(x)
