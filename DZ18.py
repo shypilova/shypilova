9893b6f70d18f0ad29876feda0fcbc7de16e55e1
Maryna.Shypilova <shypilovamarina@gmail.com>
2024-03-15 18:23:44 +0100

    dz18

--- DZ18.py
+++ DZ18.py
@@ -1,25 +1,14 @@
-list_1 = (0, 2, 3, 5, 6, 9, 15, 30)
-list_2 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 30)
-
+list_1 = []
+list_2 = []
 def common_elements(first_list, second_list):
+    first_list = []
+    second_list = []
+    for i in range(0, 31, 3):
+        first_list.append(i)
+    for i in range(0, 31, 5):
+        second_list.append(i)
     set_a = set(first_list)
     set_b = set(second_list)
-    list_new_1 = list()
-    list_new_2 = list()
-    for el in first_list:
-        if (el % 3) == 0:
-            list_new_1.append(el)
-        else:
-            continue
-    for el in second_list:
-        if (el % 5) == 0:
-            list_new_2.append(el)
-        else:
-            continue
-    set_a = set(list_new_1)
-    set_b = set(list_new_2)
     diff_set = set_a.intersection(set_b)
     return diff_set
-
-assert common_elements(list_1,list_2) == {15, 30}
-print('OK')
+print(common_elements( list_1, list_2))
