26cbdd6e156ae530862eb20ad3ebf799c4c86b51
Maryna.Shypilova <shypilovamarina@gmail.com>
2024-03-19 23:12:15 +0100

    dz21

--- DZ21.py
+++ DZ21.py
@@ -1,7 +1,12 @@
 def popular_words(text, *args):
     my_text = text.lower().split()
-    result = ({b: my_text.count(b.lower()) for b in ['i', 'was', 'three', 'near']})
+    my_args = list(args)
+    lst = list()
+    for el in args[0]:
+        my_list = lst.append(el)
+    result = ({b: my_text.count(b) for b in lst})
     return result
+
 
 assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}, 'Test1'
 print('OK')
