82a46e757c4681bc758466bb2741cec446822cb4
Maryna.Shypilova <shypilovamarina@gmail.com>
2024-03-27 18:22:50 +0100

    DZ25

---
+++ DZ25.py
@@ -0,0 +1,20 @@
+def generate_cube_numbers(end):
+    cube_numbers = []
+    gen = list(range(2, end+1))
+    for number in gen:
+        number = number ** 3
+        if number > end:
+            break
+        else:
+            cube_numbers.append(number)
+        yield number
+
+
+from inspect import isgenerator
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+print("OK")
