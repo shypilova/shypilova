83ab94c6cc61eace4a38f9ef75cd817b5a2a6367
Maryna.Shypilova <shypilovamarina@gmail.com>
2024-04-02 22:38:53 +0200

    DZ28

--- new.py
+++ new.py
@@ -1,58 +1,70 @@
-class Counter:
+class Human:
 
-   def __init__(self, current=1, min_value=0, max_value=10):
-       self.current = current
-       self.min_value = min_value
-       self.max_value = max_value
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
 
-   def set_current(self, start):
-       self.current = start
-
-   def set_max(self, max_max):
-       self.max_value = max_max
-
-   def set_min(self, min_min):
-       self.min_value = min_min
-
-   def step_up(self):
-       while self.current < self.max_value:
-           self.current += 1
-           return self.current
-       if self.current == self.max_value:
-            raise ValueError('Достигнут максимум')
+    def __str__(self):
+        return f"gender : {self.gender}, age : {self.age}, first_name : {self.first_name}, last_name : {self.last_name}"
 
 
+class Student(Human):
 
-   def step_down(self):
-       while self.current > self.min_value:
-           self.current -= 1
-           return self.current
-       if self.current == self.min_value:
-           raise ValueError('Достигнут минимум')
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
 
-   def get_current(self):
-       return self.current
-
-counter = Counter()
-counter.set_current(7)
-counter.step_up()
-counter.step_up()
-counter.step_up()
-assert counter.get_current() == 10, 'Test1'
-try:
-    counter.step_up()  # ValueError
-except ValueError as e:
-    print(e) # Достигнут максимум
-assert counter.get_current() == 10, 'Test2'
+    def __str__(self):
+        return f"{self.gender}, {self.age}, {self.first_name}, {self.last_name}, {self.record_book} \n  "
 
 
-counter.set_min(7)
-counter.step_down()
-counter.step_down()
-counter.step_down()
-assert counter.get_current() == 7, 'Test3'
-try:
-    counter.step_down()  # ValueError
-except ValueError as e:
-    print(e) # Достигнут минимум
-assert counter.get_current() == 7, 'Test4'
+class Group:
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+        self.group_new = set()
+
+    def add_student(self, student):
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        for students in self.group:
+            if last_name in str(students):
+                continue
+            else:
+                self.group = self.group_new.add(students)
+                return self.group_new
+
+
+    def find_student(self, last_name):
+        for students in self.group:
+            if last_name in str(students):
+                return students
+            else:
+                return None
+
+
+    def __str__(self):
+        all_students = ''
+        for students in self.group:
+            all_students += f"{str(students)}"
+        return f'Number:{self.number}\n  {all_students} '
+
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+print(gr)
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+
+gr.delete_student('Taylor')
+print(gr)  # Only one student
+
+gr.delete_student('Taylor')  # No error!
